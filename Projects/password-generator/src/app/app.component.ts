bf773d2d48eb2d1d46ec70225a623ebe6355f7b7
Ahmad Alhazmi <104063444+AhmadAlhazmi75@users.noreply.github.com>
2024-01-19 10:48:10 +0300

    1. First Project on Angular

---
+++ Projects/password-generator/src/app/app.component.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  password = '';
+
+  useLetters = false;
+  useNumbers = false;
+  useSymbols = false;
+  passwordLength = 0;
+
+  onChangeLength(event: any) {
+    const parsedValue = parseInt(event.target.value);
+
+    if (!isNaN(parsedValue)) {
+      this.passwordLength = parsedValue;
+    }
+  }
+
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if (this.useLetters) {
+      validChars += letters;
+    }
+    if (this.useNumbers) {
+      validChars += numbers;
+    }
+    if (this.useSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.passwordLength; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+  }
+
+  onChangeUseLetters() {
+    this.useLetters = !this.useLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.useNumbers = !this.useNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.useSymbols = !this.useSymbols;
+  }
+}
